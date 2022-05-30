5cb8c4bebe881685b1c556c55b620c82d0230611
Noah <noah.clark500@gmail.com>
2022-05-30 14:22:27 -0500

    Module 1 Assignment

---
+++ FirstResponsiveWebAppClark/FirstResponsiveWebAppClark/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using FirstResponsiveWebAppClark.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstResponsiveWebAppClark.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.FV = null;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(FutureAgeModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.FV = model.AgeThisYear();
+            }
+            else
+            {
+                ViewBag.FV = null;
+            }
+            return View(model);
+        }
+    }
+}
---
+++ FirstResponsiveWebAppClark/FirstResponsiveWebAppClark/Models/FutureAgeModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace FirstResponsiveWebAppClark.Models
+{
+    public class FutureAgeModel
+    {
+        [Required (ErrorMessage = "Please enter your name")]
+        public String Name { get; set; }
+
+        [Required (ErrorMessage = "Please enter your birth year")]
+        [Range (1900, 2022, ErrorMessage ="Please enter a year between 1900 and 2022")]
+        public int? BirthYear { get; set; }
+
+        public String AgeThisYear()
+        {
+            const int CURRENT_YEAR = 2022;
+            int? age = CURRENT_YEAR - BirthYear;
+            return "Hello " + Name + " at the end of the year, you will be " + age.ToString();
+        }
+    }
+}
